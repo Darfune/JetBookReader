9621d17243718da549a22ad78faeebe7b08272b2
Darfune <harolep@gmail.com>
2022-09-21 00:21:07 +0300

    Main Activity Navigates to SplashScreen

--- app/src/main/java/com/example/jetbookreader/MainActivity.kt
+++ app/src/main/java/com/example/jetbookreader/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetbookreader.ui.theme.JetBookReaderTheme
+import com.example.jetbookreader.ui.theme.navigation.ReaderNavigation
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,7 +26,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetBookReaderTheme { }
+            JetBookReaderTheme {
+                ReaderApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun ReaderApp() {
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 46.dp)
+    ) {
+        Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+            ReaderNavigation()
         }
     }
 }
