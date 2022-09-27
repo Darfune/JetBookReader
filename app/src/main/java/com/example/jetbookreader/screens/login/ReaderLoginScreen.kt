63d3fac1ad12e241354eaf45d6e443daf52857a1
Darfune <harolep@gmail.com>
2022-09-27 12:38:03 +0300

    login Screen ui completed

--- app/src/main/java/com/example/jetbookreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/jetbookreader/screens/login/ReaderLoginScreen.kt
@@ -3,8 +3,10 @@ package com.example.jetbookreader.screens.login
 import android.text.method.SingleLineTransformationMethod
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -19,7 +21,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -28,22 +32,48 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.jetbookreader.R
 import com.example.jetbookreader.screens.splash.EmailInputField
 import com.example.jetbookreader.screens.splash.PasswordInputField
 import com.example.jetbookreader.screens.splash.ReaderLogo
 
 @Composable
 fun LoginScreen(navController: NavController) {
+
+    val showLoginForm = rememberSaveable { mutableStateOf(true) }
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
             ReaderLogo()
-            UserForm(loading = false, isCreateAccount = false) {
-                email, pass ->
-                Log.d("Form", "LoginScreen: $email @password")
-            }
+            if (showLoginForm.value)
+                UserForm(loading = false, isCreateAccount = false) { email, pass ->
+                    //Todo: login to firebase account
+                }
+            else
+                UserForm(loading = false, isCreateAccount = true) { email, pass ->
+                    //Todo: create Firebase account
+                }
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Row(
+            modifier = Modifier.padding(15.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val text = if (showLoginForm.value) "Sign up" else "Login"
+            Text(text = "New User?")
+            Text(text,
+                modifier = Modifier
+                    .clickable {
+                        showLoginForm.value = !showLoginForm.value
+                    }
+                    .padding(start = 5.dp),
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.secondaryVariant
+            )
         }
     }
 }
@@ -72,6 +102,11 @@ fun UserForm(
         .verticalScroll(rememberScrollState())
 
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        if (isCreateAccount) Text(
+            text = stringResource(id = R.string.create_acct),
+            modifier = Modifier.padding(4.dp)
+        )
+        else Text(text = "")
         EmailInputField(emailState = email,
             enabled = !loading,
             onAction = KeyboardActions {
@@ -86,6 +121,34 @@ fun UserForm(
                 if (!valid) return@KeyboardActions
                 onDone(email.value.trim(), password.value.trim())
             })
+        SubmitButton(
+            textId = if (isCreateAccount) "Create Account" else "Login",
+            loading = loading,
+            validInputs = valid
+        ) {
+            onDone(email.value.trim(), password.value.trim())
+            keyboardController?.hide()
+        }
+    }
+}
+
+@Composable
+fun SubmitButton(
+    textId: String,
+    loading: Boolean,
+    validInputs: Boolean,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        enabled = !loading && validInputs,
+        shape = CircleShape
+    ) {
+        if (loading) CircularProgressIndicator(modifier = Modifier.size(25.dp))
+        else Text(text = textId, modifier = Modifier.padding(5.dp))
     }
 }
 
