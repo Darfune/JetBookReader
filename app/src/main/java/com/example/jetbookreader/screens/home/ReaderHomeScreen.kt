db234d3d7d6edaec8223212a15db8897642febaa
jason <dasd>
2022-10-07 23:38:48 +0300

    Added Content

--- app/src/main/java/com/example/jetbookreader/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/example/jetbookreader/screens/home/ReaderHomeScreen.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,12 +19,16 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.jetbookreader.model.MBook
 import com.example.jetbookreader.navigation.ReaderScreens
+import com.example.jetbookreader.screens.splash.FABContent
+import com.example.jetbookreader.screens.splash.ReaderAppBar
+import com.example.jetbookreader.screens.splash.TitleSection
 import com.google.firebase.auth.FirebaseAuth
 
 @Preview
@@ -50,29 +51,42 @@ fun HomeScreen(navController: NavController = NavController(LocalContext.current
 
 @Composable
 fun HomeContent(navController: NavController) {
+
+    val currentUserName =
+        if (!FirebaseAuth.getInstance().currentUser?.email.isNullOrEmpty())
+            FirebaseAuth.getInstance().currentUser?.email
+                ?.split("@")?.get(0) // remove the @email.xyz
+        else "N/A"
+
     Column(
         Modifier.padding(2.dp),
-        verticalArrangement = Arrangement.SpaceEvenly
+        verticalArrangement = Arrangement.Top
     ) {
         Row(modifier = Modifier.align(alignment = Alignment.Start)) {
             TitleSection(label = "Your reading\n activity right now")
-        }
-    }
-}
-
-@Composable
-fun TitleSection(
-    modifier: Modifier = Modifier,
-    label: String
-) {
-    Surface(modifier.padding(start = 5.dp, top = 1.dp)) {
-        Column {
-            Text(
-                text = label,
-                fontSize = 19.sp,
-                fontStyle = FontStyle.Normal,
-                textAlign = TextAlign.Left
-            )
+            Spacer(modifier = Modifier.fillMaxWidth(0.7f))
+            Column {
+                Icon(
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate(ReaderScreens.ReaderStatsScreen.name)
+                        }
+                        .size(45.dp),
+                    tint = MaterialTheme.colors.secondaryVariant
+                )
+                Text(
+                    text = currentUserName!!,
+                    modifier = Modifier.padding(2.dp),
+                    style = MaterialTheme.typography.overline,
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Clip
+                )
+                Divider()
+            }
         }
     }
 }
@@ -83,61 +97,3 @@ fun ReadingRightNowArea(books: List<MBook>, navController: NavController) {
 }
 
 
-@Composable
-fun FABContent(onTap: (String) -> Unit) {
-    FloatingActionButton(
-        onClick = { onTap },
-        shape = RoundedCornerShape(50.dp),
-        backgroundColor = Color(0xFF92CBDF)// MaterialTheme.colors.background
-    ) {
-        Icon(
-            imageVector = Icons.Default.Add,
-            contentDescription = "Add a Book",
-            tint = Color.White// MaterialTheme.colors.onSecondary
-        )
-    }
-}
-
-
-@Composable
-fun ReaderAppBar(
-    title: String, showProfile: Boolean = true, navController: NavController
-) {
-    TopAppBar(
-        title = {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                if (showProfile) {
-                    Image(imageVector = Icons.Default.Favorite,
-                        contentDescription = "Logo Icon",
-                        modifier = Modifier
-                            .clickable { RoundedCornerShape(12.dp) }
-                            .scale(0.6f))
-                }
-                Text(
-                    text = title,
-                    color = Color.Red.copy(alpha = 0.7f),
-                    style = TextStyle(
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp
-                    )
-                )
-                Spacer(modifier = Modifier.width(150.dp))
-            }
-        },
-        actions = {
-            IconButton(onClick = {
-                FirebaseAuth.getInstance().signOut().run {
-                    navController.navigate(ReaderScreens.LoginScreen.name)
-                }
-            }) {
-                Icon(
-                    imageVector = Icons.Filled.Logout,
-                    contentDescription = "Logout",
-                    tint = Color.Green.copy(alpha = 0.4f)
-                )
-            }
-        },
-        backgroundColor = Color.Transparent,
-        elevation = 0.dp
-    )
-}
