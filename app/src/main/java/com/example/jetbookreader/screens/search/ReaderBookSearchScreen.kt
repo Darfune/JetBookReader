998f350ce1a55c2ec66eecfebc33c11968655e39
Darfune <harolep@gmail.com>
2022-10-08 22:09:16 +0300

    Added top bar and search field

--- app/src/main/java/com/example/jetbookreader/screens/search/ReaderBookSearchScreen.kt
+++ app/src/main/java/com/example/jetbookreader/screens/search/ReaderBookSearchScreen.kt
@@ -1,9 +1,74 @@
 package com.example.jetbookreader.screens.search
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.example.jetbookreader.navigation.ReaderScreens
+import com.example.jetbookreader.screens.splash.InputField
+import com.example.jetbookreader.screens.splash.ReaderAppBar
 
+@Preview
 @Composable
-fun SearchScreen(navController: NavHostController) {
-    
+fun SearchScreen(navController: NavController = NavController(LocalContext.current)) {
+    Scaffold(topBar = {
+        ReaderAppBar(
+            title = "Search Books",
+            icon = Icons.Default.ArrowBack,
+            showProfile = false,
+            navController = navController
+        ) {
+            navController.navigate(ReaderScreens.ReaderHomeScreen.name)
+        }
+    }) {
+        Surface() {
+            Column {
+                SearchForm(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SearchForm(
+    modifier: Modifier = Modifier,
+    loading: Boolean = false,
+    hint: String = "Search",
+    onSearch: (String) -> Unit = {}
+) {
+    Column() {
+        val searchQueryState = rememberSaveable { mutableStateOf("") }
+        val keyboardController = LocalSoftwareKeyboardController.current
+        val valid = remember(searchQueryState.value) { searchQueryState.value.trim().isNotBlank() }
+
+        InputField(valueState = searchQueryState, labelId = "Search", enabled = true,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onSearch(searchQueryState.value.trim())
+                searchQueryState.value = ""
+                keyboardController?.hide()
+            }
+        )
+    }
 }
