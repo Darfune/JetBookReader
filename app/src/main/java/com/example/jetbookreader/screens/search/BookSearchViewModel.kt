4b5ae6ea582fbefa19590d396ee2034b4fc19cd2
Darfune <harolep@gmail.com>
2022-10-09 23:03:10 +0300

    Book search viewModel

---
+++ app/src/main/java/com/example/jetbookreader/screens/search/BookSearchViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.jetbookreader.screens.search
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetbookreader.data.Resource
+import com.example.jetbookreader.model.googleapimodel.Item
+import com.example.jetbookreader.repository.BookRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BookSearchViewModel @Inject constructor(private val repository: BookRepository) :
+    ViewModel() {
+    var list: List<Item> by mutableStateOf(listOf())
+
+    init {
+        loadBooks()
+    }
+
+    private fun loadBooks() {
+        searchBooks("android")
+    }
+
+    fun searchBooks(query: String) {
+        viewModelScope.launch(Dispatchers.Default) {
+            if (query.isEmpty()) {
+                return@launch
+            }
+            try {
+                when (val response = repository.getBooks(query)) {
+                    is Resource.Success -> list = response.data!!
+                    is Resource.Error -> Log.e("Network", "searchBooks: Failed getting books")
+                    else -> {}
+                }
+            } catch (exception: Exception) {
+                Log.d("Network", "searchBooks: ${exception.message.toString()}")
+            }
+        }
+    }
+}
