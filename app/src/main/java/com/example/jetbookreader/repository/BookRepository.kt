2af354e30c6ab2be27a51e1f07eb3caf29d021e7
Darfune <harolep@gmail.com>
2022-10-09 22:36:55 +0300

    Books repo

--- app/src/main/java/com/example/jetbookreader/repository/BookRepository.kt
+++ app/src/main/java/com/example/jetbookreader/repository/BookRepository.kt
@@ -1,33 +1,31 @@
 package com.example.jetbookreader.repository
 
-import com.example.jetbookreader.data.DataOrException
+import com.example.jetbookreader.data.Resource
 import com.example.jetbookreader.model.googleapimodel.Item
 import com.example.jetbookreader.network.BooksApi
 import javax.inject.Inject
 
 class BookRepository @Inject constructor(private val api: BooksApi) {
-    private val dataOrException = DataOrException<List<Item>, Boolean, Exception>()
-    private val bookInfoDataException = DataOrException<Item, Boolean, Exception>()
 
-    suspend fun getBooks(searchQuery: String): DataOrException<List<Item>, Boolean, Exception> {
-        try {
-            dataOrException.loading = true
-            dataOrException.data = api.getAllBooks(searchQuery).items
-            if (dataOrException.data!!.isNotEmpty()) dataOrException.loading = false
-        } catch (e: Exception) {
-            dataOrException.e = e
+    suspend fun getBooks(searchQuery: String): Resource<List<Item>> {
+        return try {
+            Resource.Loading(data = true)
+            val itemList = api.getAllBooks(searchQuery).items
+            if (itemList.isNotEmpty()) Resource.Loading(data = false)
+            Resource.Success(data = itemList)
+        } catch (exception: Exception) {
+            Resource.Error(message = exception.message.toString())
         }
-        return dataOrException
     }
 
-    suspend fun getBookInfo(bookId: String): DataOrException<Item, Boolean, Exception> {
+    suspend fun getBookInfo(bookId: String): Resource<Item> {
         val response = try {
-            dataOrException.loading = true
-            bookInfoDataException.data = api.getBookInfo(bookId = bookId)
-            if (bookInfoDataException.data.toString().isNotEmpty()) bookInfoDataException.loading = false else {}
-        } catch (e: Exception) {
-            bookInfoDataException.e = e
+            Resource.Loading(data = true)
+            api.getBookInfo(bookId)
+        } catch (exception: Exception) {
+            return Resource.Error(message = "An error occurred: ${exception.message.toString()}")
         }
-        return bookInfoDataException
+        Resource.Loading(data = false)
+        return Resource.Success(data = response)
     }
 }
