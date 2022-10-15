6f686cf019293db334252576a2c9ceb2cf4bf72e
Darfune <harolep@gmail.com>
2022-10-15 23:55:12 +0300

    Update book Screen added stuff

--- app/src/main/java/com/example/jetbookreader/screens/update/ReaderUpdateScreen.kt
+++ app/src/main/java/com/example/jetbookreader/screens/update/ReaderUpdateScreen.kt
@@ -1,23 +1,26 @@
 package com.example.jetbookreader.screens.update
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.jetbookreader.data.DataOrException
 import com.example.jetbookreader.model.MBook
 import com.example.jetbookreader.screens.home.HomeScreenViewModel
@@ -66,8 +69,90 @@ fun UpdateScreen(
                     LinearProgressIndicator()
                     bookInfo.loading = false
                 } else {
-                    Text(text = viewModel.data.value.data?.get(0)?.title.toString())
+                    Surface(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .fillMaxWidth(),
+                        shape = CircleShape,
+                        elevation = 4.dp
+                    ) {
+                        ShowBookUpdate(bookInfo = viewModel.data.value, bookItemId = bookItemId)
+                    }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowBookUpdate(bookInfo: DataOrException<List<MBook>, Boolean, Exception>, bookItemId: String) {
+    Row {
+        Spacer(modifier = Modifier.width(43.dp))
+        if (bookInfo.data != null) {
+            Column(
+                modifier = Modifier.padding(4.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                CardListItem(book = bookInfo.data!!.first { mBook ->
+                    mBook.googleBookId == bookItemId
+                }, onPressDetails = {})
+            }
+        }
+    }
+}
+
+@Composable
+fun CardListItem(book: MBook, onPressDetails: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(
+                start = 4.dp,
+                end = 4.dp,
+                top = 4.dp,
+                bottom = 8.dp
+            )
+            .clip(RoundedCornerShape(20.dp))
+            .clickable { },
+        elevation = 8.dp
+    ) {
+        Row(horizontalArrangement = Arrangement.Start) {
+            Image(
+                painter = rememberAsyncImagePainter(model = book.photoUrl.toString()),
+                contentDescription = "Book image",
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(120.dp)
+                    .padding(4.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 20.dp,
+                            topEnd = 20.dp,
+                            bottomEnd = 0.dp,
+                            bottomStart = 0.dp
+                        )
+                    )
+            )
+            Column {
+                Text(
+                    text = book.title.toString(),
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
+                        .width(120.dp),
+                    fontWeight = FontWeight.SemiBold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = book.publishedDate.toString(),
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(
+                        start = 8.dp,
+                        end = 8.dp,
+                        top = 0.dp,
+                        bottom = 8.dp
+                    )
+                )
             }
         }
     }
